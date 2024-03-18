00c6c0251b1913957d457f86766b76eb2190e12c
hamzaesswidi <esswidihamza5@gmail.com>
2024-03-18 15:10:05 +0000

    ADD ALL APIS

--- app.js
+++ app.js
@@ -8,7 +8,26 @@ app.get("/", (req, res) => {
     res.send("Obtenir la demande lancée avec succès");
 })
 
+app.post("/post", (req, res) => {
+    console.log("POST REQUEST SUCCESSFUL");
+    console.log(req.body);
+    res.send("Data POST Request Recieved");
+  });
+
+
+  app.put("/put", (req, res) => {
+    console.log("PUT REQUEST SUCCESSFUL");
+    console.log(req.body);
+    res.send("Data Update Request Recieved");
+  });
+
 app.listen(PORT, () => {
     console.log('Server established at ${PORT}');
 
 })
+app.delete("/delete", (req, res) => {
+    console.log("DELETE REQUEST SUCCESSFUL");
+    console.log(req.body);
+    res.send("Data DELETE Request Recieved");
+  });
+
